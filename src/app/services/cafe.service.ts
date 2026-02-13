c1072bcc36c5578d104b825d160ab82357f37267
BryamsSantizo <bryamssantizo1993@gmail.com>
2026-02-13 10:58:41 -0600

    Create cafe.service.ts

---
+++ src/app/services/cafe.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_BASE } from '../config/api-base';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CafeService {
+  // Asegúrate de que esta URL coincida con tu Backend en Spring Boot
+  private apiUrl = '${API_BASE}/auth/api/bolsa/cafe-hoy';
+
+  constructor(private http: HttpClient) {}
+
+  getPrecioBolsa(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
